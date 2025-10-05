45ffc81f2d382639d045d3e384125ba0f34d62df
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2025-10-05 18:48:40 +0200

    push

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-﻿# main.py (à la racine du projet)
+﻿# main.py
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# importe la coroutine main depuis le package du bot
-from services.bot.src.main import main as bot_main
 import asyncio
+from fastapi import FastAPI
+import uvicorn
+from services.bot.src.main import main as bot_main
+
+app = FastAPI()
+
+@app.get("/")
+def ping():
+    return {"status": "ok"}
 
 if __name__ == "__main__":
-    # lance proprement la coroutine principale du bot
-    asyncio.run(bot_main())
+    loop = asyncio.get_event_loop()
+    # lance le bot dans une tâche background
+    loop.create_task(bot_main())
+    # lance le serveur FastAPI
+    uvicorn.run(app, host="0.0.0.0", port=8080)
